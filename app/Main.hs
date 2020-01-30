98337be025f5cf075802f9bb57454d27f3357bd9
IchiroKawashima <kawashima.ichiro172@mail.kyutech.jp>
2020-01-30 18:09:25 +0900

    added natNormalizeExample

--- app/Main.hs
+++ app/Main.hs
@@ -7,3 +7,4 @@ import           Lib
 main :: IO ()
 main = do
     print $ knownNatExample (Proxy @3)
+    print (natNormalizeExample @5 @7)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib
     ( knownNatExample
+    , natNormalizeExample
     )
 where
 
@@ -9,3 +10,7 @@ import           Data.Proxy
 knownNatExample :: forall (n :: Nat) . (KnownNat n) => Proxy n -> Integer
 knownNatExample _ = natVal (Proxy @n) + natVal (Proxy @(n + 2))
 
+natNormalizeExample :: forall (x :: Nat) (y :: Nat)
+                     . (KnownNat x, KnownNat y)
+                    => Proxy ((x + 2) ^ (y + 2))
+natNormalizeExample = Proxy :: Proxy (4 * x * (2 + x) ^ y + 4 * (2 + x) ^ y + (2 + x) ^ y * x ^ 2)
