f1d3934efb8a8eaf71b41eb4111b287bfd6c10b7
IchiroKawashima <kawashima.ichiro172@mail.kyutech.jp>
2020-01-30 18:33:13 +0900

    added extraExample

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,12 @@
 module Lib
     ( knownNatExample
     , natNormalizeExample
+    , extraExample
     )
 where
 
 import           GHC.TypeLits
+import           GHC.TypeLits.Extra
 import           Data.Proxy
 
 knownNatExample :: forall (n :: Nat) . (KnownNat n) => Integer
@@ -14,3 +16,6 @@ natNormalizeExample :: forall (x :: Nat) (y :: Nat)
                      . (KnownNat x, KnownNat y)
                     => Proxy ((x + 2) ^ (y + 2))
 natNormalizeExample = Proxy :: Proxy (4 * x * (2 + x) ^ y + 4 * (2 + x) ^ y + (2 + x) ^ y * x ^ 2)
+
+extraExample :: forall (x :: Nat) (y :: Nat) . (KnownNat x, KnownNat y) => Proxy (x ^ Log x y)
+extraExample = Proxy :: Proxy y
